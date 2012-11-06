a09cc40bd2e744be68ecf57f1e7c795f75a8679b
hisaichi5518 <info@moe-project.com>
2012-11-06 11:22:07 +0900

    multi_postとpostに分けた

--- lib/WWW/ImKayac/AnyEvent.pm
+++ lib/WWW/ImKayac/AnyEvent.pm
@@ -6,6 +6,7 @@ use AnyEvent;
 use AnyEvent::HTTP;
 use WWW::ImKayac::Util;
 use HTTP::Request::Common qw(POST);
+use JSON::XS qw(decode_json);
 
 has base_uri => (
     is => 'rw',
@@ -15,42 +16,54 @@ has base_uri => (
 
 no Mouse;
 
-sub post {
+sub multi_post {
+    my $cb = pop @_;
     my ($self, @user_info) = @_;
 
-    my @data;
     my $cv = AnyEvent->condvar;
     for my $user (@user_info) {
         $cv->begin;
-        my $authtype = $user->{authtype};
-        my $username = $user->{username};
-        my $password = $user->{password};
-        my $message  = $user->{message};
+        $self->post(%$user, cb => sub {
+            $cb->(@_);
+            $cv->end;
+        });
+    }
+    $cv->recv;
+}
 
-        my @params = WWW::ImKayac::Util::build_params(
+sub post {
+    my ($self, %args) = @_;
+    my $authtype = $args{authtype};
+    my $username = $args{username};
+    my $password = $args{password};
+    my $message  = $args{message};
+    my $cb = $args{cb};
+
+    my @params = WWW::ImKayac::Util::build_params(
+        authtype => $authtype,
+        username => $username,
+        password => $password,
+        message  => $message,
+    );
+
+    my $req = POST $self->base_uri."/$username", \@params;
+    http_post $req->uri, $req->content, headers => $req->headers, sub {
+        my ($body, $headers) = @_;
+
+        my $json = eval { decode_json $body };
+        $cb->($@, $json, {
             authtype => $authtype,
             username => $username,
             password => $password,
             message  => $message,
-        );
-
-        my $req = POST $self->base_uri."/$username", \@params;
-        http_post $req->uri, $req->content, headers => $req->headers, sub {
-            my ($body, $headers) = @_;
-            push @data, {
-                authtype => $authtype,
-                username => $username,
-                password => $password,
-                message  => $message,
-                body     => $body,
-                headers  => $headers
-            };
-            $cv->end;
-        };
-    }
-    $cv->recv;
-
-    return @data;
+            body     => $body,
+            headers  => $headers,
+        });
+    };
 }
 
 __PACKAGE__->meta->make_immutable;
+
+
+
+
